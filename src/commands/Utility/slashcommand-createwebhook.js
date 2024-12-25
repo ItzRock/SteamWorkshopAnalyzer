d8da721d4046e60cbb1966094b58c4a7df4029bd
Anthony Stainton <anthonystainton+contact@gmail.com>
2024-12-24 21:13:27 -0800

    a

--- src/commands/Utility/slashcommand-createwebhook.js
+++ src/commands/Utility/slashcommand-createwebhook.js
@@ -1,11 +1,6 @@
 const { ChatInputCommandInteraction, EmbedBuilder, PermissionFlagsBits, ApplicationCommandOptionType } = require("discord.js");
 const DiscordBot = require("../../client/DiscordBot");
 const ApplicationCommand = require("../../structure/ApplicationCommand");
-const os = require("os")
-const moment = require("moment");
-require("moment-duration-format");
-function formatBytes(a,b=2){if(0===a)return"0 Bytes";const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));return parseFloat((a/Math.pow(1024,d)).toFixed(c))+""+["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][d]}
-
 module.exports = new ApplicationCommand({
     command: {
         name: 'createwebhook',
