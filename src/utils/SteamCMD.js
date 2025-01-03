c958eae78b699be33fb4c3c8579019385cea1c2c
Anthony Stainton <anthonystainton+contact@gmail.com>
2025-01-03 13:31:52 -0800

    changed linux commands

--- src/utils/SteamCMD.js
+++ src/utils/SteamCMD.js
@@ -1,16 +1,9 @@
-const { spawn } = require("child_process");
+const { spawn, exec } = require("child_process");
 const downloadWorkshopItem = (appid, fileid) => {
     const platform = process.platform;
     let steamcmdPath;
 
-    if (platform === "linux") {
-        steamcmdPath = "steamcmd"; // assuming steamcmd is a global
-    } else if (platform === "win32") {
-        steamcmdPath = "./bin/steamcmd/steamcmd.exe";
-    } else {
-        console.warn("SteamCMD is not supported on this platform.");
-        return;
-    }
+    let steamcmd
 
     const args = [
         "+force_install_dir", "../output",
@@ -19,7 +12,19 @@ const downloadWorkshopItem = (appid, fileid) => {
         "+quit"
     ];
 
-    const steamcmd = spawn(steamcmdPath, args);
+    if (platform === "linux") {
+        steamcmdPath = "steamcmd "; // assuming steamcmd is a global
+
+        steamcmd = exec(steamcmdPath + args.join(" "), (error, stdout, stderr)=> {
+            
+        });
+    } else if (platform === "win32") {
+        steamcmdPath = "./bin/steamcmd/steamcmd.exe";
+        steamcmd = spawn(steamcmdPath, args);
+    } else {
+        console.warn("SteamCMD is not supported on this platform.");
+        return;
+    }
 
     steamcmd.on("error", (error) => {
         console.error(`Failed to start SteamCMD: ${error.message}`);
